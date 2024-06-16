5259c5d23569a6827f5ab646fb5bb6143ab60112
Kapil Maheshwari <mkapil678@gmail.com>
2024-06-17 00:32:00 +0530

    Statements, White Space And Indenting

---
+++ 42_StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int myVariable = 50; //this complete line is a statement.
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                "another" +
+                " test.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("myVariable = " + myVariable);
+    }
+}
